1e9b2c09fd0e5e1fd950415c5c813d29d86d72be
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-02-16 18:48:00 +0700

    input: nt36xxx: Disable NVT_DEBUG
    basically, log generated by nvt is like this:
    
    [ 3212.827510] msm-dsi-panel:[dsi_panel_reset:414] [NVT-ts] lcd-reset_gpio = 1
    [ 3212.837653] msm-dsi-panel:[dsi_panel_reset:414] [NVT-ts] lcd-reset_gpio = 0
    [ 3212.847792] msm-dsi-panel:[dsi_panel_reset:414] [NVT-ts] lcd-reset_gpio = 1
    [ 3213.090939] [NVT-ts] nvt_drm_notifier_callback 2707: event=1, *blank=0
    [ 3213.091108] [NVT-ts] nvt_ts_resume 2600: start
    [ 3213.091118] [NVT-ts] update_firmware_request 313: filename is novatek_ts_tianma_fw.bin
    [ 3213.099447] [NVT-ts] nvt_get_fw_need_write_size 70: fw_need_write_size = 110592(0x1b000), NVT end flag
    [ 3213.099459] [NVT-ts] nvt_bin_header_parser 177: ovly_info = 0, ilm_dlm_num = 2, ovly_sec_num = 0, info_sec_num = 13, partition = 15
    [ 3213.232546] [NVT-ts] nvt_update_firmware 905: Update firmware success! <133072 us>
    [ 3213.233804] [NVT-ts] nvt_get_fw_info 777: FW type is 0x01
    [ 3213.240348] [NVT-ts] nvt_read_pid 717: PID=591F
    [ 3213.252221] [NVT-ts] nvt_irq_enable 270: enable=1, desc->depth=0
    [ 3213.252236] [NVT-ts] nvt_ts_resume 2647: end
    [ 3459.933374] [NVT-ts] nvt_drm_notifier_callback 2700: event=2, *blank=1
    [ 3459.933400] [NVT-ts] nvt_irq_enable 270: enable=0, desc->depth=1
    [ 3459.933406] [NVT-ts] nvt_ts_suspend 2535: start
    [ 3459.985456] [NVT-ts] nvt_ts_suspend 2579: end
    
    plus lots of:
    
    [ 3460.616474] [NVT-ts] nvt_pm_suspend 2751: pm suspend
    [ 3460.694994] [NVT-ts] nvt_pm_resume 2762: pm resume
    
    which is unused and spamming dmesg, disable it.
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Change-Id: I2a7cf2a53d9d17fd935b73176dabe1a2e0dcd429
    Signed-off-by: Abhishek001konni <Abhishek001konni@gmail.com>

--- drivers/input/touchscreen/mediatek/NT36672C/nt36xxx.h
+++ drivers/input/touchscreen/mediatek/NT36672C/nt36xxx.h
@@ -49,7 +49,7 @@
 #define TP_SUSPEND_EN 1
 /* Huaqin modify for HQ-131657 by liunianliang at 2021/06/16 end */
 
-#define NVT_DEBUG 1
+#define NVT_DEBUG 0
 /*BSP.Tp - 2020.11.05 -add NVT_LOCKDOWN - start*/
 #define NVT_LOCKDOWN 1
 /*BSP.Tp - 2020.11.05 -add NVT_LOCKDOWN, end*/
@@ -74,9 +74,11 @@ void nvt_lockdown_proc_deinit(void);
 #define NVT_SPI_NAME "NVT-ts"
 
 #if NVT_DEBUG
-#define NVT_LOG(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
+#define NVT_LOG(fmt, args...)                                                  \
+	pr_info("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
 #else
-#define NVT_LOG(fmt, args...)    pr_info("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
+#define NVT_LOG(fmt, args...)                                                  \
+	pr_debug("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
 #endif
 #define NVT_ERR(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
 
