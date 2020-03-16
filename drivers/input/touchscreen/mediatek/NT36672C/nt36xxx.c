19beb87c880f4f633e03483b30018a43213dfa7f
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-03-16 23:30:51 +0700

    touchscreen: nt36xxx: Minor code cleanup
    Change-Id: If799b1022a0a6f33a8e88c84a51bee1cbf5d4fd6
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

--- drivers/input/touchscreen/mediatek/NT36672C/nt36xxx.c
+++ drivers/input/touchscreen/mediatek/NT36672C/nt36xxx.c
@@ -732,7 +732,6 @@ return:
 int32_t nvt_read_pid(void)
 {
 	uint8_t buf[4] = {0};
-	int32_t ret = 0;
 
 	//---set xdata index to EVENT BUF ADDR---
 	nvt_set_page(ts->mmap->EVENT_BUF_ADDR | EVENT_MAP_PROJECTID);
@@ -750,7 +749,7 @@ int32_t nvt_read_pid(void)
 
 	NVT_LOG("PID=%04X\n", ts->nvt_pid);
 
-	return ret;
+	return 0;
 }
 
 /*******************************************************
@@ -991,8 +990,7 @@ static int32_t nvt_flash_close(struct inode *inode, struct file *file)
 {
 	struct nvt_flash_data *dev = file->private_data;
 
-	if (dev)
-		kfree(dev);
+	kfree(dev);
 
 	return 0;
 }
@@ -2256,7 +2254,7 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 #endif
 
 #if WAKEUP_GESTURE
-	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++) {
+	for (retry = 0; retry < ARRAY_SIZE(gesture_key_array); retry++) {
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	}
 	ts->input_dev->event = nvt_gesture_switch;
@@ -3127,7 +3125,6 @@ static struct spi_driver nvt_spi_driver = {
 	.id_table	= nvt_ts_id,
 	.driver = {
 		.name	= NVT_SPI_NAME,
-		.owner	= THIS_MODULE,
 #ifdef CONFIG_OF
 		.of_match_table = nvt_match_table,
 #endif
