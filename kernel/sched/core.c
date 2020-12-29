49d0721ebb5e2ff244b889487c276a3bddbdec4e
wenju.xu <wenju.xu@mediatek.com>
2020-12-29 23:20:34 +0800

    [ALPS05495903] sched: init rqs walt info when cpu startup
    Init rq->window_start when cpu startup to avoid leak for rqs
    cumulative_run_avg
    
    MTK-Commit-Id: 20bb7ed9973ba17271241610deadeb155aee25e6
    
    Change-Id: I5944d0bdee95d25347d9786f756a082b5b144112
    Signed-off-by: wenju.xu <wenju.xu@mediatek.com>
    CR-Id: ALPS05495903
    Feature: Energy Aware Scheduling Plus (EAS+)
    (cherry picked from commit 69c48052c924bf20ec0f445d5325fa920e4fb269)

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7651,6 +7651,11 @@ void set_rq_offline(struct rq *rq)
 static void set_cpu_rq_start_time(unsigned int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
+	struct rq_flags rf;
+
+	rq_lock(rq, &rf);
+	walt_set_window_start(rq, &rf);
+	rq_unlock(rq, &rf);
 
 	rq->age_stamp = sched_clock_cpu(cpu);
 }
