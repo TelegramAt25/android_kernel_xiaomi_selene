711f07c1a00f54325c8904248c9350344f437a71
Danny Lin <danny@kdrag0n.dev>
2021-03-16 12:52:17 +0530

    treewide: Suppress verbose log spam
    It's hard to see anything that's actually useful with so much verbose
    spam in the log buffer.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>
    Signed-off-by: ukashah1 <ukashah60@gmail.com>
    Signed-off-by: Egii <regidesoftian@gmail.com>
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1316,7 +1316,7 @@ void enable_nonboot_cpus(void)
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
-	pr_info("Enabling non-boot CPUs ...\n");
+	pr_debug("Enabling non-boot CPUs ...\n");
 
 	arch_enable_nonboot_cpus_begin();
 
