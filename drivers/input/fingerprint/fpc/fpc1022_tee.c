3c08f630b9aa93d6709b8222b3a27e079556adc6
Vaisakh Murali <mvaisakh@statixos.com>
2021-09-05 14:38:41 +0530

    drivers: fpc: Use linux octal permissions for attributes
    Use the macros are not preferred. Use octals.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- drivers/input/fingerprint/fpc/fpc1022_tee.c
+++ drivers/input/fingerprint/fpc/fpc1022_tee.c
@@ -250,7 +250,7 @@ static ssize_t hw_reset_set(struct device *dev,
 	return ret ? ret : count;
 }
 
-static DEVICE_ATTR(hw_reset, S_IWUSR, NULL, hw_reset_set);
+static DEVICE_ATTR(hw_reset, 0200, NULL, hw_reset_set);
 
 /**
 * sysfs node for controlling whether the driver is allowed
@@ -274,7 +274,7 @@ static ssize_t wakeup_enable_set(struct device *dev,
 	return count;
 }
 
-static DEVICE_ATTR(wakeup_enable, S_IWUSR, NULL, wakeup_enable_set);
+static DEVICE_ATTR(wakeup_enable, 0200, NULL, wakeup_enable_set);
 
 /**
 * sysfs node for sending event to make the system interactive,
@@ -300,7 +300,7 @@ static ssize_t do_wakeup_set(struct device *dev,
 	return count;
 }
 
-static DEVICE_ATTR(do_wakeup, S_IWUSR, NULL, do_wakeup_set);
+static DEVICE_ATTR(do_wakeup, 0200, NULL, do_wakeup_set);
 
 static ssize_t clk_enable_set(struct device *dev,
 			      struct device_attribute *attr, const char *buf,
@@ -330,7 +330,7 @@ static ssize_t clk_enable_set(struct device *dev,
 
 }
 
-static DEVICE_ATTR(clk_enable, S_IWUSR, NULL, clk_enable_set);
+static DEVICE_ATTR(clk_enable, 0200, NULL, clk_enable_set);
 
 /**
 * sysf node to check the interrupt status of the sensor, the interrupt
@@ -357,7 +357,7 @@ static ssize_t irq_ack(struct device *device,
 	return count;
 }
 
-static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
+static DEVICE_ATTR(irq, 0600, irq_get, irq_ack);
 
 static ssize_t fpc_ic_is_exist(struct device *device,
 			       struct device_attribute *attribute, char *buffer)
@@ -371,7 +371,7 @@ static ssize_t fpc_ic_is_exist(struct device *device,
 	return scnprintf(buffer, PAGE_SIZE, "%i\n", fpc_exist);
 }
 
-static DEVICE_ATTR(fpid_get, S_IRUSR | S_IWUSR, fpc_ic_is_exist, NULL);
+static DEVICE_ATTR(fpid_get, 0600, fpc_ic_is_exist, NULL);
 
 /* K19A code for HQ-145238 by shicheng at 2021.7.12 start */
 static ssize_t fingerdown_wait_set(struct device *device,
@@ -396,8 +396,8 @@ static ssize_t fingerdown_wait_set(struct device *device,
 		return count;
 }
 
-static DEVICE_ATTR(fingerdown_wait, S_IWUSR, NULL, fingerdown_wait_set);
-/* K19A code for HQ-145238 by shicheng at 2021.7.12 end */
+static DEVICE_ATTR(fingerdown_wait, 0200, NULL, fingerdown_wait_set);
+/* end modify for unlock speed */
 
 
 static struct attribute *attributes[] = {
